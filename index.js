d7df7e388d69d2f03f5cf8e622b72724b037ce38
nicochnz <chiche.nicolas@gmail.com>
2024-11-04 15:18:11 +0100

    added new person

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 import cors from "cors";
 import express from "express";
 
-const sampleEmployee = {
-	name: {
-		first: "ntm",
-
-		last: "Thompson",
+const sampleEmployees = [
+	{
+		name: {
+			first: "Charlie",
+			last: "Thompson",
+		},
+		email: "charlie.thompson@example.com",
+		picture: {
+			medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+		},
 	},
-
-	email: "charlie.thompson@example.com",
-
-	picture: {
-		medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+	{
+		name: {
+			first: "Charline",
+			last: "Thompson",
+		},
+		email: "jane.doe@example.com",
+		picture: {
+			medium: "https://randomuser.me/api/portraits/med/women/40.jpg",
+		},
 	},
-};
+];
+
 const app = express();
 app.use(cors({ origin: ["http://localhost:5173"] }));
+
 app.get("/api/employees", (req, res) => {
-	res.json({ results: [sampleEmployee] });
+	res.json({ results: sampleEmployees });
 });
 
 const port = 3310;
