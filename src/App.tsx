772f12686d4bfdc341677255bb852d1f026bd6a4
nicochnz <chiche.nicolas@gmail.com>
2024-11-04 15:19:37 +0100

    Added new person

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,47 @@
 import "./App.css";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EmployeeCard from "./components/EmployeeCard";
 
 interface Employee {
-	name: {
-		first: string;
-		last: string;
-	};
-	email: string;
-	picture: {
-		medium: string;
-	};
+  name: {
+    first: string;
+    last: string;
+  };
+  email: string;
+  picture: {
+    medium: string;
+  };
 }
 
 export default function App() {
-	const [employee, setEmployee] = useState<Employee | null>(null);
+  const [employee, setEmployee] = useState<Employee | null>(null);
 
-	const getEmployee = () => {
-		fetch("http://localhost:3310/api/employees")
-			.then((response) => response.json())
-			.then((data) => {
-				setEmployee(data.results[0]);
-				console.log(data.results[0]);
-			});
-	};
+  const getEmployee = () => {
+    fetch("http://localhost:3310/api/employees")
+      .then((response) => response.json())
+      .then((data) => {
+        const randomEmployee =
+          data.results[Math.floor(Math.random() * data.results.length)];
+        setEmployee(randomEmployee);
+        console.log(randomEmployee);
+      });
+  };
 
-	useEffect(() => {
-		console.log("J'ai chargé un nouvel employé:", employee);
-	}, [employee]);
+  useEffect(() => {
+    console.log("J'ai chargé un nouvel employé:", employee);
+  }, [employee]);
 
-	return (
-		<div className="App">
-			<h1>Profil d'une personne aléatoire</h1>
-			{employee ? (
-				<EmployeeCard employee={employee} />
-			) : (
-				<p>Chargement du profil...</p>
-			)}
-			<button type="button" onClick={getEmployee}>
-				Obtenir un nouveau profil
-			</button>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>Profil d'une personne aléatoire</h1>
+      {employee ? (
+        <EmployeeCard employee={employee} />
+      ) : (
+        <p>Chargement du profil...</p>
+      )}
+      <button type="button" onClick={getEmployee}>
+        Obtenir un nouveau profil
+      </button>
+    </div>
+  );
 }
--- src/components/EmployeeCard.tsx
+++ src/components/EmployeeCard.tsx
@@ -1,26 +1,30 @@
 interface EmployeeCardProps {
-	employee: {
-		name: {
-			first: string;
-			last: string;
-		};
-		email: string;
-		picture: {
-			medium: string;
-		};
-	};
+  employee: {
+    name: {
+      first: string;
+      last: string;
+    };
+    email: string;
+    picture: {
+      medium: string;
+    };
+  };
 }
 
 export default function EmployeeCard({ employee }: EmployeeCardProps) {
-	return (
-		<figure>
-			<img src={employee.picture.medium} alt={employee.name.first} />
-			<figcaption>
-				<strong>
-					{employee.name.first} {employee.name.last}
-				</strong>
-				<p>{employee.email}</p>
-			</figcaption>
-		</figure>
-	);
+  if (!employee || !employee.picture) {
+    return <p>Chargement du profil...</p>;
+  }
+
+  return (
+    <figure>
+      <img src={employee.picture.medium} alt={employee.name.first} />
+      <figcaption>
+        <strong>
+          {employee.name.first} {employee.name.last}
+        </strong>
+        <p>{employee.email}</p>
+      </figcaption>
+    </figure>
+  );
 }
